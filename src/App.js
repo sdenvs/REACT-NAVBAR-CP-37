aff0077f8f6f9f4af4b9308071dc3072a866220c
sdenvs <sdenvs@gmail.com>
2022-01-13 13:16:22 +0530

    change-1

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import {Switch, Route} from 'react-router-dom'
 
 import {Component} from 'react'
 
+import ThemeContext from './context/ThemeContext'
+
 import Home from './components/Home'
 
 import About from './components/About'
@@ -11,15 +13,30 @@ import NotFound from './components/NotFound'
 import './App.css'
 
 class App extends Component {
+  state = {isDarkTheme: false}
+
+  toggleTheme = () => {
+    this.setState(prev => ({
+      isDarkTheme: !prev.isDarkTheme,
+    }))
+  }
+
   render() {
+    const {isDarkTheme} = this.state
     return (
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/about" component={About} />
-        <Route component={NotFound} />
-      </Switch>
+      <ThemeContext.Provider
+        value={{
+          isDarkTheme,
+          toggleTheme: this.toggleTheme,
+        }}
+      >
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route component={NotFound} />
+        </Switch>
+      </ThemeContext.Provider>
     )
   }
 }
-
 export default App
